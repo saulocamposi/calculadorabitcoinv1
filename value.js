797b3348bd3fa857f874f4a6b12d72f9d3763074
saulocamposi <saulocamposi@gmail.com>
2015-12-06 19:10:33 -0200

    app

--- value.js
+++ value.js
@@ -1,24 +1,53 @@
+var app = angular.module('calcbticoin', []);
 
-var app = angular.module('calcbticoin',[]);
-
-var mbCtrl = function($scope, $http){
-     $http.get("https://mercadobitcoin.net/api/ticker").then(function(response){
+var mbCtrl = function($scope, $http) {
+  /*   $http.get("https://mercadobitcoin.net/api/ticker").then(function(response){
       $scope.mbbit = response.data;
-      });
-     $http.get("https://blockchain.info/ticker").then(function(response){
-      $scope.blockchain = response.data;
-      });
+    });*/
+
+  $http.get("https://blockchain.info/ticker").then(function(response) {
+    $scope.blockchain = response.data;
+  });
+
+  $http.get("https://api.github.com/users/pabloleary").then(function(response) {
+    $scope.git = response.data;
+  });
+
+  $http.get("https://api.github.com/users/pabloleary").then(function(response) {
+    $scope.foxbit = response.data;
+  });
+
+  $http.get("https://www.bitcointoyou.com/api/ticker.aspx").then(function(response) {
+    $scope.btctoyou = response.data;
+  });
 
 
 
-      $http.get("https://api.github.com/users/pabloleary").then(function(response){
-       $scope.git = response.data;
-       });
-
-       var currency = function(type){
-         return angular.equals(type, "BRL");
-       }
+  var currency = function(type) {
+    return angular.equals(type, "BRL");
+  }
 
 }
 
-app.controller("mbCtrl",["$scope","$http",mbCtrl]);
+app.directive('numericOnly', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, element, attrs, modelCtrl) {
+
+      modelCtrl.$parsers.push(function(inputValue) {
+        var transformedInput = inputValue ? inputValue.replace(/[^\d.-]/g, '') : null;
+
+        if (transformedInput != inputValue) {
+          modelCtrl.$setViewValue(transformedInput);
+          modelCtrl.$render();
+        }
+
+        return transformedInput;
+      });
+    }
+  };
+});
+
+
+
+app.controller("mbCtrl", ["$scope", "$http", mbCtrl]);
