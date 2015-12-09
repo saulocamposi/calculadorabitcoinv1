7549c2a850ed33d30c7a361feaa62039e6402a2a
saulocamposi <saulocamposi@gmail.com>
2015-12-09 02:12:05 -0200

    version 1

--- value.js
+++ value.js
@@ -1,53 +1,41 @@
-var app = angular.module('calcbticoin', []);
+var app = angular.module('calcbtc', []);
+
+app.config(['$httpProvider', function($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}]);
 
 var mbCtrl = function($scope, $http) {
-  /*   $http.get("https://mercadobitcoin.net/api/ticker").then(function(response){
-      $scope.mbbit = response.data;
-    });*/
-
-  $http.get("https://blockchain.info/ticker").then(function(response) {
+  $http.get("https://blockchain.info/ticker?cors=true").then(function(response) {
     $scope.blockchain = response.data;
   });
 
-  $http.get("https://api.github.com/users/pabloleary").then(function(response) {
+
+  $http.get("https://api.github.com/users/pabloleary?cors=true").then(function(response) {
     $scope.git = response.data;
   });
 
-  $http.get("https://api.github.com/users/pabloleary").then(function(response) {
+  $http.get("https://api.blinktrade.com/api/v1/BRL/ticker?crypto_currency=BTC?cors=true").then(function(response) {
     $scope.foxbit = response.data;
   });
 
-  $http.get("https://www.bitcointoyou.com/api/ticker.aspx").then(function(response) {
+
+  /*
+
+  $http.get("https://www.bitcointoyou.com/api/ticker.aspx?cors=true").then(function(response) {
     $scope.btctoyou = response.data;
   });
 
+  $http.get("https://mercadobitcoin.net/api/ticker?cors=true").then(function(response){
+   $scope.mbbit = response.data;
+  });
+
+*/
 
 
   var currency = function(type) {
     return angular.equals(type, "BRL");
   }
-
 }
-
-app.directive('numericOnly', function() {
-  return {
-    require: 'ngModel',
-    link: function(scope, element, attrs, modelCtrl) {
-
-      modelCtrl.$parsers.push(function(inputValue) {
-        var transformedInput = inputValue ? inputValue.replace(/[^\d.-]/g, '') : null;
-
-        if (transformedInput != inputValue) {
-          modelCtrl.$setViewValue(transformedInput);
-          modelCtrl.$render();
-        }
-
-        return transformedInput;
-      });
-    }
-  };
-});
-
-
 
 app.controller("mbCtrl", ["$scope", "$http", mbCtrl]);
