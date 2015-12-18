77ef85a421c2d38c4f407b75c459725749bbd5b7
saulocamposi <saulocamposi@gmail.com>
2015-12-18 19:02:18 -0200

    the flowBTC was inserted and MTC was realign

--- valuep.js
+++ valuep.js
@@ -6,21 +6,32 @@ var mbCtrl = function($scope, $http, $resource) {
     $scope.btctoyou = response.data;
   });
 
-  $http.get("services.php?brand=mtc").then(function(response) {
-    $scope.mtc = response.data;
+  $http.get("services.php?brand=flowbtc").then(function(response) {
+    $scope.flowbtc = response.data;
   });
 
   $http.get("services.php?brand=negociecoins").then(function(response) {
     $scope.negociecoins = response.data;
   });
 
+  $http.jsonp("https://api.blinktrade.com/api/v1/BRL/ticker?crypto_currency=BTC&callback=JSON_CALLBACK").then(function(response) {
+    $scope.foxbit = response.data;
+  });
+
   $http.get("bitvalor_services.php").then(function(response) {
     $scope.bitvalor = response.data;
   });
 
-  $http.jsonp("https://api.blinktrade.com/api/v1/BRL/ticker?crypto_currency=BTC&callback=JSON_CALLBACK").then(function(response) {
-    $scope.foxbit = response.data;
+  /*
+
+  API disabled per exceed limit of GETs
+
+  $http.get("services.php?brand=mtc").then(function(response) {
+    $scope.mtc = response.data;
   });
+
+  */
+
 
 
 
