b2e947575a7623e833c2089500f0293def3a41d6
saulocamposi <saulocamposi@gmail.com>
2016-02-06 06:28:09 -0200

    The Foxbitwallet card was inserted

--- services/walletfoxbit_hot_services.php
+++ services/walletfoxbit_hot_services.php
@@ -3,9 +3,30 @@
 
    $wallet =  json_decode(file_get_contents("https://blockchain.info/address/1FoxBitjXcBeZUS4eDzPZ7b124q3N7QJK7?format=json&cors=true"));
 
+/*
+   function formatBTC($value){
+     $valueBTC = substr($value);
+     $valeuSAT = substr($value);
+     return $valueBTC + " " + $valeuSAT;
+
+   }
+*/
    $received = $wallet->total_received;
+   $receivedBTC =  substr($received, 0, 3);
+   $receivedSAT =  substr($received, 3, 8);
+   $received = $receivedBTC + "," + $receivedSAT;
+
    $sent = $wallet->total_sent;
+   /*$sentBTC = substr();
+   $sentSAT = substr();
+   $sentSAT = $sentBTC + "," + $sentSAT;
+   */
    $balance = $wallet->final_balance;
+
+   /*$balanceBTC = substr();
+   $balanceSAT = substr();
+   $balaceSAT = $balaceBTC + "," + $balaceSAT;
+   */
 
    $ticker =
    array(
