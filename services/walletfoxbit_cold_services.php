affa00eef2052b2b3a9c44d1f099dc645189ef9e
saulocamposi <saulocamposi@gmail.com>
2016-02-06 07:13:30 -0200

    The Foxbitwallet card was inserted

--- services/walletfoxbit_cold_services.php
+++ services/walletfoxbit_cold_services.php
@@ -3,9 +3,20 @@
 
    $wallet =  json_decode(file_get_contents("https://blockchain.info/address/3KAuEYkuJQw1Ad2GzWjfC7V5XoL2fCqjGN?format=json&cors=true"));
 
+   function formatBTC($value){
+     $valueBTC = substr($value, 0, 3);
+     $valeuSAT = substr($value, 3, 8);
+     return $valueBTC . "," . $valeuSAT;
+   }
+
    $received = $wallet->total_received;
+   $received = formatBTC($received);
+
    $sent = $wallet->total_sent;
+   $sent = formatBTC($sent);
+
    $balance = $wallet->final_balance;
+   $balance = formatBTC($balance);
 
    $ticker =
    array(
--- services/walletfoxbit_hot_services.php
+++ services/walletfoxbit_hot_services.php
@@ -3,30 +3,20 @@
 
    $wallet =  json_decode(file_get_contents("https://blockchain.info/address/1FoxBitjXcBeZUS4eDzPZ7b124q3N7QJK7?format=json&cors=true"));
 
-/*
    function formatBTC($value){
-     $valueBTC = substr($value);
-     $valeuSAT = substr($value);
-     return $valueBTC + " " + $valeuSAT;
-
+     $valueBTC = substr($value, 0, 3);
+     $valeuSAT = substr($value, 3, 8);
+     return $valueBTC . "," . $valeuSAT;
    }
-*/
+
    $received = $wallet->total_received;
-   $receivedBTC =  substr($received, 0, 3);
-   $receivedSAT =  substr($received, 3, 8);
-   $received = $receivedBTC + "," + $receivedSAT;
+   $received = formatBTC($received);
 
    $sent = $wallet->total_sent;
-   /*$sentBTC = substr();
-   $sentSAT = substr();
-   $sentSAT = $sentBTC + "," + $sentSAT;
-   */
-   $balance = $wallet->final_balance;
+   $sent = formatBTC($sent);
 
-   /*$balanceBTC = substr();
-   $balanceSAT = substr();
-   $balaceSAT = $balaceBTC + "," + $balaceSAT;
-   */
+   $balance = $wallet->final_balance;
+   $balance = formatBTC($balance);
 
    $ticker =
    array(
