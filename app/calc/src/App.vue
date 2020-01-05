6ca1380ef7be5f7160e588caa4392dc5f8376f58
saulocamposi <saulocamposi@gmail.com>
2020-01-05 23:21:53 +0000

    Update -- new components for index

--- app/calc/src/App.vue
+++ app/calc/src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <BitValor></BitValor>
+    <Exchanges/>
+    <Footer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import BitValor from './components/BitValor.vue'
+import Exchanges from './components/Exchanges.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    Footer,
+    BitValor,
+    Exchanges
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
